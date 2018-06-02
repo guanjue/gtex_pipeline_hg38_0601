2fac18335ff35f3a3547936090eda863b97630a8
Universe <universe@MacBook-Air.local>
2018-06-01 20:59:05 -0400

    prepare ref list

--- shared_pipeline/get_gtex_human_rep.sh
+++ shared_pipeline/get_gtex_human_rep.sh
@@ -1,19 +1,3 @@
-###### Dependence
-### R
-# metap
-### python
-# numpy, matplotlib, scipy
-
-script_folder=$1
-input_folder=$2
-ref_ref=$3
-### H3K4me3_H1_R1
-mark_ref_cell=$4
-### H1
-mark_ref_rep_id=$5
-
-cd $input_folder
-
 ###### (1) Reads-count --> (2) NB-p-value 
 for ct in $(cat cell_list.txt)
 do
@@ -30,44 +14,29 @@ do
 		done
 	done
 done
+rm file_list_tmp.txt
 
-###### (3) Prepare PKnorm list (between reference sample)
-rm 'pknorm_list_reference.rep.txt'
+
+###### extract all reference list
 for mk in $(cat mark_list.txt)
 do
 	echo $mk
-	### select the reference sample for different mark reference pknorm normalization
-	echo $ref_ref'.nbp_2r_bgadj.txt' >> 'pknorm_list.1.txt'
-	echo $mk'_'$mark_ref_cell'_'$mark_ref_rep_id'.nbp_2r_bgadj.txt' >> 'pknorm_list.2.txt'
+	ls *$mk*.signal.tab.mvsp.txt > $mk'.file_list.txt'
+	time Rscript $script_folder'get_mk_ref.R' $mk'.file_list.txt' $mk'.ref_frip.txt'
 done
-paste 'pknorm_list.1.txt' 'pknorm_list.2.txt' > 'pknorm_list_reference.rep.txt'
-rm 'pknorm_list.1.txt'
-rm 'pknorm_list.2.txt'
 
-###### (4) Prepare PKnorm list (between reference & target sample)
-for mk in $(cat mark_list.txt)
-do
-	echo $mk
-	rm $mk'.pknorm_list.rep.txt'
-	for ct in $(cat cell_list.txt)
-	do
-		echo $ct
-		echo $mk'_'$mark_ref_cell'_'$mark_ref_rep_id'.pknorm.ref.txt' >> $mk'.pknorm_list.1.txt'
-		echo $mk'_'$ct'_R1.nbp_2r_bgadj.txt' >> $mk'.pknorm_list.2.txt'
-		echo $mk'_'$mark_ref_cell'_'$mark_ref_rep_id'.pknorm.ref.txt' >> $mk'.pknorm_list.1.txt'
-		echo $mk'_'$ct'_R2.nbp_2r_bgadj.txt' >> $mk'.pknorm_list.2.txt'
-	done
-	paste $mk'.pknorm_list.1.txt' $mk'.pknorm_list.2.txt' > $mk'.pknorm_list.rep.txt'
-	rm $mk'.pknorm_list.1.txt' 
-	rm $mk'.pknorm_list.2.txt'
-done
+
+###### get all reference list
+cat *.ref_frip.txt | sort -k2,2rn > all_ref_list.txt
+ref0=$(head -1 all_ref_list.txt | awk -F '.' -v OFS='\t' '{print $1"."$2"."$3"."$4".nbp_2r_bgadj.txt"}')
+
 
 ###### (5) PKnorm normalization (between reference sample)
 while read LINE
 do
-	sig1=$(echo "$LINE" | awk '{print $1}')
-	sig2=$(echo "$LINE" | awk '{print $2}')
-	sig2_celltype=$(echo "$LINE" | awk '{print $2}' | awk -F '.' -v OFS='\t' '{print $1}')
+	sig1=$ref0
+	sig2=$(echo "$LINE" | awk -F '.' -v OFS='\t' '{print $1"."$2"."$3"."$4".nbp_2r_bgadj.txt"}')
+	sig2_celltype=$(echo "$LINE" | awk -F '\t' -v OFS='\t' '{print $1}' | awk -F '.' -v OFS='\t' '{print $1}')
 	upperlim=500
 	lowerlim=0
 	echo $sig1 
@@ -80,21 +49,25 @@ do
 	time python $script_folder'peaknorm_rotate_log_ref_mean.py' -w 1 -p 1 -n 500000 -a 1 -b $sig1'.upperlim.txt' -c 1 -d $sig2'.upperlim.txt' -u $upperlim -l $lowerlim
 	### rm tmp files
 	rm $sig1'.upperlim.txt' $sig2'.upperlim.txt'
-	mv $sig2_celltype'_nbp_2r_bgadj.pknorm.txt' $sig2_celltype'.pknorm.ref.txt'
-	mv $sig2_celltype'_nbp_2r_bgadj.info.txt' $sig2_celltype'.info.ref.txt'
-	mv $sig2_celltype'_nbp_2r_bgadj.pknorm.scatterplot.png' $sig2_celltype'.pknorm.scatterplot.ref.png'
-	mv $sig2_celltype'_nbp_2r_bgadj.scatterplot.png' $sig2_celltype'.scatterplot.ref.png'
-done < pknorm_list_reference.rep.txt
+	mv $sig2_celltype'.pknorm.txt' $sig2_celltype'.pknorm.ref.txt'
+	mv $sig2_celltype'.info.txt' $sig2_celltype'.info.ref.txt'
+	mv $sig2_celltype'.pknorm.scatterplot.png' $sig2_celltype'.pknorm.scatterplot.ref.png'
+	mv $sig2_celltype'.scatterplot.png' $sig2_celltype'.scatterplot.ref.png'
+done < all_ref_list.txt
+
 
 ###### (6) PKnorm normalization (between reference & target sample)
 for mk in $(cat mark_list.txt)
 do
 	echo $mk
+	ref_mk=$(head -1 $mk'.ref_frip.txt' | awk -F '.' -v OFS='\t' '{print $1".pknorm.ref.txt"}')
+	echo $ref_mk
+	ls $ref_mk
 	while read LINE
 	do
-		sig1=$(echo "$LINE" | awk '{print $1}')
-		sig2=$(echo "$LINE" | awk '{print $2}')
-		sig2_celltype=$(echo "$LINE" | awk '{print $2}' | awk -F '.' -v OFS='\t' '{print $1}')
+		sig1=$ref_mk
+		sig2=$(echo "$LINE" | awk -F '.' -v OFS='\t' '{print $1".bam.signal.tab.nbp_2r_bgadj.txt"}')
+		sig2_celltype=$(echo "$LINE" | awk -F '.' -v OFS='\t' '{print $1}')
 		upperlim=500
 		lowerlim=0
 		echo $sig1 
@@ -108,15 +81,15 @@ do
 		### rm tmp files
 		rm $sig1'.upperlim.txt' $sig2'.upperlim.txt'
 		cat $sig2_celltype'.pknorm.txt' | awk -F '\t' -v OFS='\t' '{if ($1>=16) print 16; else if ($1<=2) print 2; else print $1}' > $sig2_celltype'.pknorm.2_16lim.txt'
-	done < $mk'.pknorm_list.rep.txt'
+	done < $mk'.file_list.txt'
 done
 
-mkdir vision_data_output_2_16lim/
-mv *.pknorm.2_16lim.txt vision_data_output_2_16lim/
+mkdir entex_data_output_2_16lim/
+mv *.pknorm.2_16lim.txt entex_data_output_2_16lim/
 
 
 ###### write ideas input file
-ls vision_data_output_2_16lim/ > vision_pknorm_2_16lim_filelist.txt
+ls entex_data_output_2_16lim/ > entex_pknorm_2_16lim_filelist.txt
 ### remove previous ideas.input file
 if [ -f ideas.input ]
 then
@@ -124,14 +97,23 @@ then
 	rm ideas.input
 fi
 ### get new ideas.input file
-for filename in $(cat vision_pknorm_2_16lim_filelist.txt)
+for filename in $(cat entex_pknorm_2_16lim_filelist.txt)
 do
 	echo $filename
-	mk=$(echo "$filename" | awk -F '_' '{print $1}')
-	ct=$(echo "$filename" | awk -F '_' '{print $2}')
+	mk=$(echo "$filename" | awk -F '_' '{print $2}' | awk -F '-' '{print $1}')
+	exp=$(echo "$filename" | awk -F '_' '{print $1}' | awk -F '-' '{print $1}')
+	if [ $exp = "DNase" ]; then mk=$(echo "$filename" | awk -F '_' '{print $1}' | awk -F '-' '{print $1}');fi
+	ct=$(echo "$filename" | awk -F '_' '{print $4}')
 	rep=$(echo "$filename" | awk -F '.' '{print $1}' | awk -F '_' '{print $3}')
-	echo $ct'_'$rep $mk $input_folder'vision_data_output_2_16lim/'$filename >> ideas.input
+	echo $ct $mk $input_folder'entex_data_output_2_16lim/'$filename >> ideas.input
 done
+
+
+
+
+
+
+
 
 
 
